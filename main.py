4326121ab8864eb9e28b4265783a73b8110fdb20
AlikaMinakova <94383130+AlikaMinakova@users.noreply.github.com>
2021-12-26 09:52:26 +0300

    start

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-#проект
+import pygame.sprite
+import pygame
+import os
+import sys
+from pygame.locals import *
+
+
+pygame.init()
+size = width, height = 750, 550
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+all_sprites = pygame.sprite.Group()
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+running = True
+play = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pressed_keys = pygame.key.get_pressed()
+    screen.fill(pygame.Color("black"))
+    pygame.display.flip()
+    clock.tick(50)
+pygame.quit()
