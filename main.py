457abc93a8d03ab161988fed1dc2d8b19c3c6651
AlikaMinakova <94383130+AlikaMinakova@users.noreply.github.com>
2022-01-02 10:53:58 +0300

    commit_g2

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 all_sprites = pygame.sprite.Group()
 playerfirst = pygame.sprite.Group()
+playersecond = pygame.sprite.Group()
+cris_group = pygame.sprite.Group()
+cris_group2 = pygame.sprite.Group()
 
 
 def load_image(name, colorkey=None):
@@ -29,23 +32,95 @@ def load_image(name, colorkey=None):
     return image
 
 
+girl_left = [load_image('left_g.png'), load_image('left2_g.png'), load_image('left_g.png'),
+             load_image('left2_g.png')]
+girl_right = [load_image('right_g.png'), load_image('right2_g.png'), load_image('right_g.png'),
+             load_image('right3_g.png')]
+
+
 class PlayerFirst(pygame.sprite.Sprite):
     image = load_image("girl.png", colorkey=-1)
 
-    def __init__(self):
+    def __init__(self, x, y):
         super().__init__(playerfirst, all_sprites)
         self.image = PlayerFirst.image
-        self.pol = 461
+        self.pol = y
+        self.count = 0
         self.isJump = False
         self.jumpCount = 12
         self.jumpCount1 = 0
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
-        self.rect.x = 0
+        self.rect.x = x
         self.rect.y = self.pol
 
+    def move(self, pressed_keys):
+        global SCORE
+        self.image = load_image("girl.png", colorkey=-1)
+        if pygame.sprite.spritecollide(self, cris_group, True):
+            SCORE += 10
+        if pressed_keys[K_RIGHT]:
+            self.rect.x += 4
+            if self.rect.x > 700:
+                self.rect.x -= 4
+            self.animated(girl_right)
+            if pygame.sprite.spritecollide(self, ground_im, False):
+                if 459 <= self.rect.y <= 465:
+                    self.rect.x -= 4
+        if pressed_keys[K_LEFT]:
+            self.rect.x -= 4
+            if self.rect.x < -10:
+                self.rect.x += 4
+            self.animated(girl_left)
+            if pygame.sprite.spritecollide(self, ground_im, False):
+                if 459 <= self.rect.y <= 465:
+                    self.rect.x -= 4
+        if self.isJump:
+            if self.jumpCount > 0:
+                self.rect.y -= self.jumpCount
+                self.jumpCount -= 1
+            else:
+                self.down()
+        else:
+            if not pygame.sprite.spritecollide(self, platform_ground, False):
+                if not pygame.sprite.spritecollide(self, ground_im, False):
+                    self.isdown()
 
-first = PlayerFirst()
+    def down(self):
+        if pygame.sprite.spritecollide(self, platform_ground, False):
+            self.rect.y += 5
+            self.isJump = False
+            self.jumpCount = 12
+            self.jumpCount1 = 0
+
+        elif pygame.sprite.spritecollide(self, ground_im, False):
+            self.rect.y += 2
+            self.isJump = False
+            self.jumpCount = 12
+            self.jumpCount1 = 0
+
+        else:
+            if self.jumpCount1 <= 12:
+                self.rect.y += self.jumpCount1
+                self.jumpCount1 += 1
+            else:
+                self.isJump = False
+                self.jumpCount = 12
+                self.jumpCount1 = 0
+
+    def isdown(self):
+        if self.rect.y < self.pol:
+            self.rect.y += 5
+
+    def animated(self, img):
+        if self.count == 20:
+            self.count = 0
+
+        self.image = img[self.count // 5]
+        self.count += 1
+
+
+first = PlayerFirst(40, 461)
 running = True
 play = True
 while running:
@@ -56,6 +131,7 @@ while running:
             if event.key == pygame.K_UP:
                 first.isJump = True
     pressed_keys = pygame.key.get_pressed()
+    first.move(pressed_keys)
     screen.fill(pygame.Color("black"))
     playerfirst.draw(screen)
     pygame.display.flip()
